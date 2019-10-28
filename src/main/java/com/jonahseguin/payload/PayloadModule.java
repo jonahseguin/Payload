ec89b6a82c1fcb990edf2eef8f137137d0d11231
Jonah <me@jonahseguin.com>
2019-10-28 16:03:21 -0600

    Make Api more direct

--- src/main/java/com/jonahseguin/payload/PayloadModule.java
+++ src/main/java/com/jonahseguin/payload/PayloadModule.java
@@ -7,15 +7,12 @@ package com.jonahseguin.payload;
 
 import com.google.common.base.Preconditions;
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.jonahseguin.payload.base.CacheModule;
 import com.jonahseguin.payload.base.data.PayloadLocal;
 import com.jonahseguin.payload.base.lang.PayloadLangController;
 import com.jonahseguin.payload.base.uuid.UUIDService;
 import com.jonahseguin.payload.command.PCommandHandler;
 import com.jonahseguin.payload.database.DatabaseModule;
-import com.jonahseguin.payload.database.PayloadDatabase;
-import com.jonahseguin.payload.server.ServerManager;
 import org.bukkit.plugin.Plugin;
 
 import javax.annotation.Nonnull;
@@ -23,12 +20,14 @@ import javax.annotation.Nonnull;
 public class PayloadModule extends AbstractModule {
 
     private final PayloadPlugin payloadPlugin;
+    private final PayloadAPI api;
     private final Plugin plugin;
 
     public PayloadModule(@Nonnull PayloadPlugin payloadPlugin, @Nonnull Plugin plugin) {
         Preconditions.checkNotNull(payloadPlugin);
         Preconditions.checkNotNull(plugin);
         this.payloadPlugin = payloadPlugin;
+        this.api = payloadPlugin.getApi();
         this.plugin = plugin;
     }
 
@@ -37,18 +36,13 @@ public class PayloadModule extends AbstractModule {
         bind(PayloadPlugin.class).toInstance(payloadPlugin);
         bind(PayloadLangController.class).toInstance(payloadPlugin.getLangController());
         bind(Plugin.class).toInstance(plugin);
-        bind(PayloadAPI.class).toInstance(payloadPlugin.getApi());
+        bind(PayloadAPI.class).toInstance(api);
         bind(PCommandHandler.class).toInstance(payloadPlugin.getCommandHandler());
         bind(PayloadLocal.class).toInstance(payloadPlugin.getLocal());
         bind(UUIDService.class);
 
-        install(new DatabaseModule(payloadPlugin.getApi(), payloadPlugin, plugin));
+        install(new DatabaseModule(api, payloadPlugin, plugin));
         install(new CacheModule());
-    }
-
-    @Provides
-    ServerManager provideServerManager(PayloadDatabase database) {
-        return database.getServerManager();
     }
 
 }
