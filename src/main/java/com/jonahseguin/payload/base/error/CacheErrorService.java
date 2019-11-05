d8e94a1a9ccf49d09496c3608a3ef9c986895df5
Jonah <me@jonahseguin.com>
2019-11-05 10:58:19 -0700

    Abstract error service

--- src/main/java/com/jonahseguin/payload/base/error/CacheErrorService.java
+++ src/main/java/com/jonahseguin/payload/base/error/CacheErrorService.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019 Jonah Seguin.  All rights reserved.  You may not modify, decompile, distribute or use any code/text contained in this document(plugin) without explicit signed permission from Jonah Seguin.
+ * www.jonahseguin.com
+ */
+
 package com.jonahseguin.payload.base.error;
 
 import com.google.inject.Inject;
@@ -15,8 +20,8 @@ import javax.annotation.Nullable;
 
 public class CacheErrorService<K, X extends Payload<K>, N extends NetworkPayload<K>, D extends PayloadData> implements ErrorService, LangModule {
 
-    private final PayloadCache<K, X, N, D> cache;
-    private final LangService lang;
+    protected final PayloadCache<K, X, N, D> cache;
+    protected final LangService lang;
 
     @Inject
     public CacheErrorService(PayloadCache<K, X, N, D> cache, LangService lang) {
@@ -38,97 +43,106 @@ public class CacheErrorService<K, X extends Payload<K>, N extends NetworkPayload
     }
 
     @Override
-    public void capture(@Nonnull Throwable throwable) {
+    public String capture(@Nonnull Throwable throwable) {
         String s = lang.module(this).format("error-generic", cache.getName(), throwable.getMessage());
         cache.getPlugin().getLogger().severe(s);
         if (cache.isDebug()) {
             throwable.printStackTrace();
             cache.alert(PayloadPermission.DEBUG, s);
         }
+        return s;
     }
 
     @Override
-    public void capture(@Nonnull Throwable throwable, @Nonnull String msg) {
+    public String capture(@Nonnull Throwable throwable, @Nonnull String msg) {
         String s = lang.module(this).format("error-specific", cache.getName(), throwable.getMessage(), msg);
         cache.getPlugin().getLogger().severe(s);
         if (cache.isDebug()) {
             throwable.printStackTrace();
             cache.alert(PayloadPermission.DEBUG, s);
         }
-
+        return s;
     }
 
     @Override
-    public void capture(@Nonnull String msg) {
+    public String capture(@Nonnull String msg) {
         String s = lang.module(this).format("error-generic", cache.getName(), msg);
         cache.getPlugin().getLogger().severe(s);
         if (cache.isDebug()) {
             cache.alert(PayloadPermission.DEBUG, s);
         }
+        return s;
     }
 
     @Override
-    public void capture(@Nonnull String module, @Nonnull String key, @Nullable Object... args) {
+    public String capture(@Nonnull String module, @Nonnull String key, @Nullable Object... args) {
         String s = lang.module(module).format(key, args);
         cache.getPlugin().getLogger().severe(s);
         if (cache.isDebug()) {
             cache.alert(PayloadPermission.DEBUG, s);
         }
+        return s;
     }
 
     @Override
-    public void capture(@Nonnull Throwable throwable, @Nonnull String module, @Nonnull String key, @Nullable Object... args) {
-        String s = lang.module(module).format(key, args);
-        cache.getPlugin().getLogger().severe(s);
-        if (cache.isDebug()) {
-            cache.alert(PayloadPermission.DEBUG, s);
-            throwable.printStackTrace();
-        }
-    }
-
-    @Override
-    public void capture(@Nonnull LangModule module, @Nonnull String key, @Nullable Object... args) {
-        String s = lang.module(module).format(key, args);
-        cache.getPlugin().getLogger().severe(s);
-        if (cache.isDebug()) {
-            cache.alert(PayloadPermission.DEBUG, s);
-        }
-    }
-
-    @Override
-    public void capture(@Nonnull Throwable throwable, @Nonnull LangModule module, @Nonnull String key, @Nullable Object... args) {
+    public String capture(@Nonnull Throwable throwable, @Nonnull String module, @Nonnull String key, @Nullable Object... args) {
         String s = lang.module(module).format(key, args);
         cache.getPlugin().getLogger().severe(s);
         if (cache.isDebug()) {
             cache.alert(PayloadPermission.DEBUG, s);
             throwable.printStackTrace();
         }
+        return s;
     }
 
     @Override
-    public void debug(@Nonnull String msg) {
+    public String capture(@Nonnull LangModule module, @Nonnull String key, @Nullable Object... args) {
+        String s = lang.module(module).format(key, args);
+        cache.getPlugin().getLogger().severe(s);
+        if (cache.isDebug()) {
+            cache.alert(PayloadPermission.DEBUG, s);
+        }
+        return s;
+    }
+
+    @Override
+    public String capture(@Nonnull Throwable throwable, @Nonnull LangModule module, @Nonnull String key, @Nullable Object... args) {
+        String s = lang.module(module).format(key, args);
+        cache.getPlugin().getLogger().severe(s);
+        if (cache.isDebug()) {
+            cache.alert(PayloadPermission.DEBUG, s);
+            throwable.printStackTrace();
+        }
+        return s;
+    }
+
+    @Override
+    public String debug(@Nonnull String msg) {
         String s = lang.module(this).format("debug", cache.getName(), msg);
         cache.getPlugin().getLogger().info(s);
         if (cache.isDebug()) {
             cache.alert(PayloadPermission.DEBUG, s);
         }
+        return s;
     }
 
     @Override
-    public void debug(@Nonnull String module, @Nonnull String key, @Nullable Object... args) {
+    public String debug(@Nonnull String module, @Nonnull String key, @Nullable Object... args) {
         String s = lang.module(module).format(key, args);
         cache.getPlugin().getLogger().info(s);
         if (cache.isDebug()) {
             cache.alert(PayloadPermission.DEBUG, s);
         }
+        return s;
     }
 
     @Override
-    public void debug(@Nonnull LangModule module, @Nonnull String key, @Nullable Object... args) {
+    public String debug(@Nonnull LangModule module, @Nonnull String key, @Nullable Object... args) {
         String s = lang.module(module).format(key, args);
         cache.getPlugin().getLogger().info(s);
         if (cache.isDebug()) {
             cache.alert(PayloadPermission.DEBUG, s);
         }
+        return s;
     }
 }
--- src/main/java/com/jonahseguin/payload/base/error/ErrorService.java
+++ src/main/java/com/jonahseguin/payload/base/error/ErrorService.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019 Jonah Seguin.  All rights reserved.  You may not modify, decompile, distribute or use any code/text contained in this document(plugin) without explicit signed permission from Jonah Seguin.
+ * www.jonahseguin.com
+ */
+
 package com.jonahseguin.payload.base.error;
 
 import com.jonahseguin.lang.LangModule;
@@ -7,24 +12,24 @@ import javax.annotation.Nullable;
 
 public interface ErrorService {
 
-    void capture(@Nonnull Throwable throwable);
+    String capture(@Nonnull Throwable throwable);
 
-    void capture(@Nonnull Throwable throwable, @Nonnull String msg);
+    String capture(@Nonnull Throwable throwable, @Nonnull String msg);
 
-    void capture(@Nonnull String msg);
+    String capture(@Nonnull String msg);
 
-    void capture(@Nonnull String module, @Nonnull String key, @Nullable Object... args);
+    String capture(@Nonnull String module, @Nonnull String key, @Nullable Object... args);
 
-    void capture(@Nonnull Throwable throwable, @Nonnull String module, @Nonnull String key, @Nullable Object... args);
+    String capture(@Nonnull Throwable throwable, @Nonnull String module, @Nonnull String key, @Nullable Object... args);
 
-    void capture(@Nonnull LangModule module, @Nonnull String key, @Nullable Object... args);
+    String capture(@Nonnull LangModule module, @Nonnull String key, @Nullable Object... args);
 
-    void capture(@Nonnull Throwable throwable, @Nonnull LangModule module, @Nonnull String key, @Nullable Object... args);
+    String capture(@Nonnull Throwable throwable, @Nonnull LangModule module, @Nonnull String key, @Nullable Object... args);
 
-    void debug(@Nonnull String msg);
+    String debug(@Nonnull String msg);
 
-    void debug(@Nonnull String module, @Nonnull String key, @Nullable Object... args);
+    String debug(@Nonnull String module, @Nonnull String key, @Nullable Object... args);
 
-    void debug(@Nonnull LangModule module, @Nonnull String key, @Nullable Object... args);
+    String debug(@Nonnull LangModule module, @Nonnull String key, @Nullable Object... args);
 
 }
